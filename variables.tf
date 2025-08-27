dc5461f4d2d35e94b50f5893d2c6f004b3c0f02a
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-27 21:30:08 +0300

    added variables.tf with defined vars

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "container_name" {
+  type = string
+}
+
+variable "blob_name" {
+  type = string
+}
